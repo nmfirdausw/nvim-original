c897199fc6df55c6821c1540c88d829e34140da5
nmfirdausw <nmfirdausw@gmail.com>
2023-10-01 22:54:25 +0800

    backup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,71 @@
 return {
-  "projekt0n/caret.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('caret').setup({
-      -- ...
-    })
 
-    vim.cmd('colorscheme caret')
-  end,
+  -- {
+  --   "projekt0n/caret.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('caret').setup({
+  --       -- ...
+  --     })
+  --
+  --     vim.cmd('colorscheme caret')
+  --   end,
+  -- },
+  --   {
+  --
+  --   "neanias/everforest-nvim",
+  --   version = false,
+  --   lazy = false,
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function()
+  --     require("everforest").setup({
+  --       vim.cmd('colorscheme everforest')
+  --     })
+  --   end,
+  -- },
+
+  --   {
+  --
+  --   "savq/melange-nvim",
+  --   version = false,
+  --   lazy = false,
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function()
+  --       vim.cmd('colorscheme melange')
+  --   end,
+  -- },
+
+
+    {
+
+    "sainnhe/gruvbox-material",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+        vim.cmd("colorscheme gruvbox-material")
+    end,
+  }
+  --
+
+    -- {
+    -- "rebelot/kanagawa.nvim",
+    --     lazy = false,
+    --     priority = 1000000,
+    --     config = function()
+    --         require("kanagawa").setup({
+    --             transparent = true,
+    --             theme = "dragon",
+    --             background = {
+    --                 dark = "dragon",
+    --                 light = "dragon",
+    --             },
+    --         })
+    --         vim.cmd("colorscheme kanagawa-dragon")
+    --     end,
+    -- },
 }
